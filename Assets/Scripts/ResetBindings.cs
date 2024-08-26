ef1435ebe774f56976327c2e9fa769a4800b1585
Oscar Devs <ohernandezba@gmail.com>
2024-08-26 13:04:04 +0200

    Reset Bindings

--- Assets/Scripts/ResetBindings.cs
+++ Assets/Scripts/ResetBindings.cs
@@ -1,7 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ResetBindings : MonoBehaviour
-{    
+{
+    public InputActionAsset inputActionAsset;
+    [SerializeField] private string targetControlScheme;
+
+    public void ResetSchemeBindings()
+    {
+        foreach (InputActionMap map in inputActionAsset.actionMaps)
+        {
+            foreach (InputAction action in map.actions)
+            {
+                action.RemoveBindingOverride(InputBinding.MaskByGroup(targetControlScheme));
+            }
+        }
+    }
 }
